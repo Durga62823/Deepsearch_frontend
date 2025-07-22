57b804df37388917d04c098d1ae367ee370be41e
Durga prasad <psivadurgaprasad88@gmail.com>
2025-07-22 20:13:57 +0530

    frontend completed

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL // removed /api
+
 
 const api = axios.create({
   baseURL: API_URL,
@@ -9,6 +10,7 @@ const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
+  withCredentials: true,
 });
 
 api.interceptors.request.use(config => {
@@ -24,7 +26,6 @@ api.interceptors.request.use(config => {
   return Promise.reject(error);
 });
 
-// Add an Axios response interceptor to handle token expiration and other 401 errors globally
 api.interceptors.response.use(
   response => response, // If the response is successful (2xx), just return it
   async error => {
